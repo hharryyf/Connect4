5ea34379870b17cf834d435e85fab69c057e3f76
hharryyf <harryheyifan99@gmail.com>
2022-09-19 23:18:09 +0800

    start the python player

---
+++ Python_Player/connect4_game.py
@@ -0,0 +1,112 @@
+import numpy as np
+import torch
+from random import randint
+
+class Board(object):
+    # initialize the board
+    def __init__(self, nrow=6,ncol=7):
+        self.nrow = nrow
+        self.ncol = ncol
+        # a move dictionary key = (i, j) coordinate, value = player 
+        self.movedict = {}
+        self.col = np.zeros(ncol, dtype=int)
+        self.piece = np.zeros((nrow, ncol), dtype=int)
+        self.lastmove = -1
+        # player 1 always moves first
+        self.current_player = 1 
+        self.status = 2
+
+    # check if a player can move at column "move"
+    def can_move(self, move):
+        if move < self.ncol and move >= 0 and self.col[move] <= 5:
+            return True
+        return False
+
+    # see if there's a winner on the board
+    def has_winner(self):
+        if self.status != 2:
+            return True, self.status
+        return False, 2
+
+    # see if the game ends, either someone wins the game or 
+    # the game ends up in a draw
+    def game_end(self):
+        return self.status != 2
+
+    # the current player takes a move at column "move"
+    # precondition, the game status is not 'end'
+    def do_move(self, move):
+        assert (self.game_end() == False)
+        move = int(move)
+        def ok(tx, ty):
+            return tx >= 0 and tx <= self.nrow - 1 and ty >= 0 and ty <= self.ncol - 1
+        
+        if self.can_move(move):
+            self.lastmove = move
+            self.movedict[(self.col[move], move)] = self.current_player
+            self.piece[self.col[move], move] = self.current_player
+            # update the status of the board
+            x, y = self.col[move], move
+
+            for i in range(-3, 1):
+                if ok(x+i,y) and ok(x+i+1,y) and ok(x+i+2,y) and ok(x+i+3,y):
+                    v = self.piece[x+i, y] + self.piece[x+i+1,y] + self.piece[x+i+2, y] + self.piece[x+i+3, y]
+                    if v == 4:
+                        self.status = 1
+                    elif v == -4:
+                        self.status = -1
+
+                if ok(x,y+i) and ok(x,y+i+1) and ok(x,y+i+2) and ok(x,y+i+3):
+                    v = self.piece[x, y+i] + self.piece[x,y+i+1] + self.piece[x, y+i+2] + self.piece[x, y+i+3]
+                    if v == 4:
+                        self.status = 1
+                    elif v == -4:
+                        self.status = -1
+                
+                if ok(x+i,y+i) and ok(x+i+1,y+i+1) and ok(x+i+2,y+i+2) and ok(x+i+3,y+i+3):
+                    v = self.piece[x+i, y+i] + self.piece[x+i+1,y+i+1] + self.piece[x+i+2, y+i+2] + self.piece[x+i+3, y+i+3]
+                    if v == 4:
+                        self.status = 1
+                    elif v == -4:
+                        self.status = -1
+
+                if ok(x+i,y-i) and ok(x+i+1,y-i-1) and ok(x+i+2,y-i-2) and ok(x+i+3,y-i-3):
+                    v = self.piece[x+i, y-i] + self.piece[x+i+1,y-i-1] + self.piece[x+i+2, y-i-2] + self.piece[x+i+3, y-i-3]
+                    if v == 4:
+                        self.status = 1
+                    elif v == -4:
+                        self.status = -1
+
+            if self.status == 2 and len(self.movedict) == self.nrow * self.ncol:
+                self.status = 0
+            # increment the total number of moves in the column
+            self.col[move] += 1
+            # the current player changes
+            self.current_player *= -1
+            return True
+        return False
+    
+    '''
+        get the board state used for the policy-value network
+        dimension[0] all the pieces played by the current player
+        dimension[1] all the pieces played by the other player
+        dimension[2] the latest piece played on the board (i.e. in this dimension only 1 pixel is 1 all the others are 0)
+        dimension[3] the player doing the current move, if it is the maximizer (i.e. if it is player 1), 
+        then the final dimension is 1, otherwise 0
+        precondition: this method can only be called before current_player actually takes the move!!!!
+        return type: a torch tensor
+    '''
+    def get_board_state(self):
+        board_state = np.zeros((4, self.nrow, self.ncol))
+        if len(self.movedict) > 0:
+            # positions of the current moves
+            board_state[0] = 1.0 * (self.piece == self.current_player)
+            # positions of the moves by the other player
+            board_state[1] = 1.0 * (self.piece == -self.current_player)
+            # indicate where the last move is played
+            board_state[2][self.col[self.lastmove] - 1, self.lastmove] = 1.0
+
+        if len(self.status) % 2 == 0:
+            board_state[3][:,:] = 1.0
+        
+        return torch.from_numpy(board_state)
---
+++ Python_Player/test_board.py
@@ -0,0 +1,104 @@
+from random import randint
+from connect4_game import Board
+
+def test_player_update_simple():
+    board = Board(6, 7)
+    assert (board.current_player == 1)
+    # take a move at position 4
+    board.do_move(4)
+    assert (len(board.movedict) == 1)
+    assert (board.current_player == -1)
+    assert (board.lastmove == 4)
+    assert (board.col[4] == 1)
+    assert (board.col.sum() == 1)
+    # take a move at position 2
+    board.do_move(2)
+    assert (len(board.movedict) == 2)
+    assert (board.current_player == 1)
+    assert (board.lastmove == 2)
+    assert (board.col[4] == 1)
+    assert (board.col[2] == 1)
+    assert (board.col.sum() == 2)
+
+def check_can_move():
+    board = Board(6, 7)
+    assert (board.can_move(4))
+    board.do_move(4)
+    assert (board.can_move(4))
+    board.do_move(4)
+    assert (board.can_move(4))
+    board.do_move(4)
+    assert (board.can_move(4))
+    board.do_move(4)
+    assert (board.can_move(4))
+    board.do_move(4)
+    assert (board.can_move(4))
+    board.do_move(4)
+    assert (not board.can_move(4))
+
+def test_player_update_end_game_col():
+    def check_end(b):
+        def ok(x, y):
+            return x >= 0 and x <= 5 and y >= 0 and y <= 6
+        
+        found = False
+        for i in range(0, 6):
+            for j in range(0, 7):
+                if b[i,j] == 0:
+                    found = True                
+                if ok(i, j) and ok(i, j+1) and ok(i, j+2) and ok(i, j+3):
+                    if b[i,j] + b[i,j+1] + b[i,j+2] + b[i,j+3] == 4:
+                        return True, 1
+                    
+                    if b[i,j] + b[i,j+1] + b[i,j+2] + b[i,j+3] == -4:
+                        return True, -1
+                if ok(i, j) and ok(i+1, j) and ok(i+2, j) and ok(i+3, j):
+                    if b[i,j] + b[i+1,j] + b[i+2,j] + b[i+3,j] == 4:
+                        return True, 1
+                    
+                    if b[i,j] + b[i+1,j] + b[i+2,j] + b[i+3,j] == -4:
+                        return True, -1
+                
+                if ok(i, j) and ok(i+1, j+1) and ok(i+2, j+2) and ok(i+3, j+3):
+                    if b[i,j] + b[i+1,j+1] + b[i+2,j+2] + b[i+3,j+3] == 4:
+                        return True, 1
+                    
+                    if b[i,j] + b[i+1,j+1] + b[i+2,j+2] + b[i+3,j+3] == -4:
+                        return True, -1
+                
+                if ok(i, j) and ok(i+1, j-1) and ok(i+2, j-2) and ok(i+3, j-3):
+                    if b[i,j] + b[i+1,j-1] + b[i+2,j-2] + b[i+3,j-3] == 4:
+                        return True, 1
+                    
+                    if b[i,j] + b[i+1,j-1] + b[i+2,j-2] + b[i+3,j-3] == -4:
+                        return True, -1
+
+        if found:
+            return False, 2
+        return True, 0
+
+    # we run 10000 random games
+    for i in range(1, 10001):
+        board = Board(6, 7)
+        while True:
+            rnd = randint(0, 6)
+            if board.can_move(rnd):
+                board.do_move(rnd)    
+            
+            my_state = board.has_winner()
+            state = check_end(board.piece)
+
+            if my_state[0] != state[0] or my_state[1] != state[1]:
+                print("Wrong answer on testcase", i)
+                print("---------- Test Details -------------")
+                print("move dictionary")
+                print(board.movedict)
+                print(board.piece)
+                print(board.col)
+                print("printed status", my_state)
+
+            assert (my_state[0] == state[0])
+            assert (my_state[1] == state[1])
+
+            if state[0]:
+                break
